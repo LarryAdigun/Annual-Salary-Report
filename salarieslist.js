e158fd9705d748b599afebf3c656bfb665ec8f96
Larry Adigun <larryadigun.student@careerdevs.com>
2019-02-16 17:51:23 +0000

    New

--- salarieslist.js
+++ salarieslist.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var salary = require("./salaries.js");
+
+var fs = require("fs")
 
 // fs.readFile('salaries.js', 'utf8', function(err, salaries) {
 //     if (err) throw err;
@@ -11,14 +11,28 @@ var salary = require("./salaries.js");
 
 //console.log(salary[0].salary);
 
-  for (var i= 0; i < salary.length; i++) {
-    var sum =0;
-    console.log( "The salary of", salary[i].id, " is", salary[i].salary);
-    
-}
+//   for (var i= 0; i < salary.length; i++) {
+//     var sum =0;
+//     console.log( "The salary of", salary[i].id, " is", salary[i].salary);
 
-var salaryTotal = salary.reduce(function(prev, cur) {
-  return prev + cur.salary;
-}, 0);
+// }
 
-console.log('Total Salary:', salaryTotal);
+// var salaryTotal = salary.reduce(function(prev, cur) {
+//   return prev + cur.salary;
+// }, 0);
+
+// console.log('Total Salary:', salaryTotal);
+
+
+
+
+fs.readFile("department.txt", 'utf8', function(err, data) {  
+   if (err) throw err;
+   var arraydat = data.split('\n')
+   
+   for (var i = 0; i < arraydat.length; i++) {
+   		console.log("The data on line", i+1, "is:", data.split('\n')[i]);
+   }
+   console.log(arraydat)
+   
+});
