62815a609ebdf220ec8cd2db2d7bc3fd9858902f
Larry Adigun <larryadigun.student@careerdevs.com>
2019-02-19 17:07:43 +0000

    salary

--- Addition.js
+++ Addition.js
@@ -23,40 +23,32 @@ var employeeName = [
 for (var i = 0; i < departmentId.length; i++) {
     console.log(`Department ${departmentId[i]} - ${departments[i]}:`);
 
-    for (var j = 0; j < employeeId.length; j++) {
-        console.log("Employee:", employeeName[i][j], "Id number is:", employeeId[i][j], "salary is:", salaries[i][j])
-    }
-    
-    // var sum = 0
-    // for (var k =0;  k < salaries.length ; k++) {
-    //     console.log( sum += salaries[i][j])
-    // }
-
+    for (var j = 0; j < employeeName.length; j++) {
+        var salary = salaries[i][j];
+        var employee = employeeName[i][j];
+        var id = employeeId[i][j];
+        console.log("Employee:", employee, "Id number is:", employeeId[i][j], "salary is:", salary);
+}
 }
 
-// function NestedArraySummation(arr) {
-//     var sum = 0;
-//     for (var i = 0; i < arr.length; i++) {
-//         if (typeof arr[i] == "number")
-//             sum = sum + arr[i];
-//         else if (arr[i] instanceof Array)
-//             sum = sum + NestedArraySummation(arr[i]);
-//     }
-//     return sum;
-// }
 
-// console.log(NestedArraySummation(salaries))
 
-// function salaryReport(){
-// 	var salary = 0; 
-// 	for (var y = 0; y < departmentId.length; y++) {
-		
-// 		for (var z = 0; z < salaries[y].length; z++) {
-// 			salary += salaries[y][z]
-		
-// 		}
-// 		return salary;
-// }
-// }
-// console.log(salaryReport());
+                // var sum = 0
+                // for (var k =0;  k < salaries.length ; k++) {
+                //     console.log( sum += salaries[i][j])
+                // }
 
+
+
+                // function NestedArraySummation(arr) {
+                //     var sum = 0;
+                //     for (var i = 0; i < arr.length; i++) {
+                //         if (typeof arr[i] == "number")
+                //             sum = sum + arr[i];
+                //         else if (arr[i] instanceof Array)
+                //             sum = sum + NestedArraySummation(arr[i]);
+                //     }
+                //     return sum;
+                // }
+
+                // console.log(NestedArraySummation(salaries))
