b6540b5510777bea820431ac2f29379d3c5b1787
Larry Adigun <larryadigun.student@careerdevs.com>
2019-02-21 23:07:03 +0000

    new

---
+++ Department.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+ fs.readFile("department.txt" , "utf8", function(err,data){
+   if (err) throw err
+   var newData = data.replace(/INSERT INTO dept_emp VALUES/g, "")
+     var loadDeptArray = newData.split('\n')
+     for (var i = 0; i < loadDeptArray.length; i++) {
+         if(loadDeptArray[i].slice(28,32) == '9999'){
+             console.log(loadDeptArray[i])
+         }
+     }
+})
+
--- salarieslist.js
+++ salarieslist.js
@@ -36,14 +36,8 @@ fs.readFile("department.txt", 'utf8', function(err, data) {
    console.log(arraydat)
    
 });
-fs.readFile("department.txt", 'utf8', function(err, data) {  
-   if (err) throw err;
-   var arraydat = data.split('\n')
-   
-   for (var i = 0; i < arraydat.length; i++) {
-   		console.log("The data on line", i+1, "is:", data.split('\n')[i]);
-   }
-   console.log(arraydat)
+
+
 
 
 
